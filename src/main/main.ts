1ed081cb147b81305baeb7a31d5b452407ef7b46
xushengfeng <xushengfeng_zg@163.com>
2023-06-28 13:13:53 +0800

    代码 移除旧代码

--- src/main/main.ts
+++ src/main/main.ts
@@ -2,14 +2,10 @@
 // Modules to control application life and create native browser window
 import {
     app,
-    Tray,
-    Menu,
-    clipboard,
     globalShortcut,
     BrowserWindow,
     ipcMain,
     dialog,
-    Notification,
     shell,
     nativeImage,
     nativeTheme,
@@ -18,7 +14,6 @@ import {
     desktopCapturer,
     session,
 } from "electron";
-import { Buffer } from "buffer";
 const Store = require("electron-store") as typeof import("electron-store");
 import * as path from "path";
 const run_path = path.join(path.resolve(__dirname, ""), "../../");
@@ -83,10 +78,6 @@ function renderer_path(window: BrowserWindow | Electron.WebContents, file_name: 
     }
 }
 
-if (!store.get("硬件加速")) {
-    app.disableHardwareAcceleration();
-}
-
 app.commandLine.appendSwitch("enable-experimental-web-platform-features", "enable");
 
 app.whenReady().then(() => {
@@ -95,9 +86,6 @@ app.whenReady().then(() => {
 
     // 初始化设置
     Store.initRenderer();
-
-    // tmp目录
-    if (!fs.existsSync(os.tmpdir() + "/eSearch")) fs.mkdir(os.tmpdir() + "/eSearch", () => {});
 
     // @ts-ignore
     nativeTheme.themeSource = store.get("全局.深色模式");
@@ -121,7 +109,6 @@ ipcMain.on("setting", async (event, arg, arg1, arg2) => {
         case "save_err":
             console.log("保存设置失败");
             break;
-        case "set_default_setting":
             store.clear();
             set_default_setting();
             var resolve = await dialog.showMessageBox({
@@ -270,14 +257,7 @@ async function create_main_window() {
     return window_name;
 }
 
-function main_edit(window: BrowserWindow, m: string) {
-    window.webContents.send("edit", m);
-}
-
 var search_window_l: Map<bview_id, BrowserView> = new Map();
-ipcMain.on("open_url", (event, window_name, url) => {
-    create_browser(window_name, url);
-});
 
 function set_chrome_size(pid: number) {
     let main_window = main_window_l.get(pid);
@@ -351,7 +331,6 @@ async function create_browser(window_name: number, url: string) {
     let chrome = main_to_chrome.get(window_name).view;
 
     if (main_window.isDestroyed()) return;
-    min_views(main_window);
     const view: bview_id = new Date().getTime();
     let tree_id: view_id = new Date().getTime();
     bview_view.set(view, [tree_id]);
@@ -361,7 +340,6 @@ async function create_browser(window_name: number, url: string) {
 
     let search_view = new BrowserView();
     search_window_l.set(view, search_view);
-    await search_view.webContents.session.setProxy(store.get("代理"));
     main_window.addBrowserView(search_view);
     main_window.setTopBrowserView(chrome);
     search_view.webContents.loadURL(url);
@@ -465,12 +443,6 @@ async function create_browser(window_name: number, url: string) {
 
     return tree_id;
 }
-/**
- * 标签页事件
- */
-function view_events(w: BrowserWindow, arg: string) {
-    w.webContents.send("view_events", arg);
-}
 
 ipcMain.on("tab_view", (e, id, arg, arg2) => {
     console.log(arg);
@@ -480,15 +452,6 @@ ipcMain.on("tab_view", (e, id, arg, arg2) => {
     switch (arg) {
         case "close":
             search_window.webContents.close();
-            break;
-        case "top":
-            // 有时直接把主页面当成浏览器打开，这时pid未初始化就触发top了，直接忽略
-            if (!main_window) return;
-            main_window.setTopBrowserView(search_window);
-            min_views(main_window);
-            search_window.setBounds(
-                get_size(main_window.getContentBounds().width, main_window.getContentBounds().height)
-            );
             break;
         case "back":
             search_window.webContents.goBack();
@@ -534,25 +497,11 @@ ipcMain.on("tab_view", (e, id, arg, arg2) => {
                 }
             });
             break;
-        case "home":
-            min_views(main_window);
-            break;
-        case "save_html":
-            main_window["html"] = arg2;
-            min_views(main_window);
-            break;
         case "dev":
             search_window.webContents.openDevTools();
             break;
     }
 });
-
-/** 最小化某个窗口的所有标签页 */
-function min_views(main_window: BrowserWindow) {
-    for (let v of main_window.getBrowserViews()) {
-        v.setBounds({ x: 0, y: 0, width: 0, height: 0 });
-    }
-}
 
 ipcMain.on("theme", (e, v) => {
     nativeTheme.themeSource = v;
@@ -561,232 +510,9 @@ ipcMain.on("theme", (e, v) => {
 
 // 默认设置
 var default_setting = {
-    首次运行: false,
-    设置版本: app.getVersion(),
-    启动提示: true,
-    语言: {},
-    快捷键: {
-        自动识别: {
-            key: "Alt+C",
-        },
-        截屏搜索: {},
-        选中搜索: {},
-        剪贴板搜索: {},
-        快速截屏: {},
-        主页面: {},
-    },
-    点击托盘自动截图: process.platform != "linux",
-    其他快捷键: {
-        关闭: "Escape",
-        OCR: "Enter",
-        以图搜图: "",
-        QR码: "",
-        图像编辑: isMac ? "Command+D" : "Control+D",
-        其他应用打开: "",
-        放在屏幕上: "",
-        录屏: "",
-        长截屏: "",
-        复制: isMac ? "Command+C" : "Control+C",
-        保存: isMac ? "Command+S" : "Control+S",
-        复制颜色: "K",
-    },
-    主搜索功能: {
-        自动搜索排除: [],
-        剪贴板选区搜索: true,
-        截屏搜索延迟: 0,
-    },
-    全局: {
-        模糊: 25,
-        缩放: 1,
-        不透明度: 0.4,
-        深色模式: "system",
-        图标颜色: ["", ""],
-    },
-    工具栏: {
-        按钮大小: 60,
-        按钮图标比例: 0.7,
-    },
-    字体: {
-        主要字体: "",
-        等宽字体: "",
-        记住: false,
-        大小: 16,
-    },
-    编辑器: {
-        自动换行: true,
-        拼写检查: false,
-        行号: true,
-        tab: 2,
-        光标动画: 0.05,
-    },
-    工具栏跟随: "展示内容优先",
-    取色器默认格式: "HEX",
-    自动搜索: true,
-    遮罩颜色: "#0008",
-    选区颜色: "#0000",
-    像素大小: 10,
-    取色器大小: 15,
-    显示四角坐标: true,
-    其他应用打开: "",
-    框选: {
-        自动框选: {
-            开启: false,
-            图像识别: false,
-            最小阈值: 50,
-            最大阈值: 150,
-        },
-        记忆: { 开启: false, rects: {} },
-    },
-    图像编辑: {
-        默认属性: {
-            填充颜色: "#fff",
-            边框颜色: "#333",
-            边框宽度: 1,
-            画笔颜色: "#333",
-            画笔粗细: 2,
-        },
-        复制偏移: {
-            x: 10,
-            y: 10,
-        },
-        形状属性: {},
-    },
-    OCR: {
-        类型: "默认",
-        离线切换: true,
-        记住: false,
-    },
-    离线OCR: [["默认", "默认/ppocr_det.onnx", "默认/ppocr_rec.onnx", "默认/ppocr_keys_v1.txt"]],
-    离线OCR配置: {
-        node: false,
-    },
-    在线OCR: {
-        baidu: {
-            url: "",
-            id: "",
-            secret: "",
-        },
-        youdao: {
-            id: "",
-            secret: "",
-        },
-    },
-    以图搜图: {
-        引擎: "baidu",
-        记住: false,
-    },
-    自动打开链接: false,
-    自动搜索中文占比: 0.2,
-    浏览器中打开: false,
-    浏览器: {
-        标签页: {
-            自动关闭: true,
-            小: false,
-            灰度: false,
-        },
-    },
-    保存: {
-        默认格式: "png",
-        保存路径: { 图片: "", 视频: "" },
-        快速保存: false,
-    },
-    保存名称: { 前缀: "eSearch-", 时间: "YYYY-MM-DD-HH-mm-ss-S", 后缀: "" },
-    jpg质量: 1,
-    框选后默认操作: "no",
-    快速截屏: { 模式: "clip", 路径: "" },
-    搜索引擎: [
-        ["Google", "https://www.google.com/search?q=%s"],
-        ["百度", "https://www.baidu.com/s?wd=%s"],
-        ["必应", "https://cn.bing.com/search?q=%s"],
-        ["Yandex", "https://yandex.com/search/?text=%s"],
-    ],
-    翻译引擎: [
-        ["Google", "https://translate.google.com.hk/?op=translate&text=%s"],
-        ["Deepl", "https://www.deepl.com/translator#any/any/%s"],
-        ["金山词霸", "http://www.iciba.com/word?w=%s"],
-        ["百度", "https://fanyi.baidu.com/#auto/auto/%s"],
-        ["腾讯", "https://fanyi.qq.com/?text=%s"],
-        ["翻译树", "https://fanyishu.netlify.app/?text=%s"],
-    ],
-    引擎: {
-        记住: false,
-        默认搜索引擎: "百度",
-        默认翻译引擎: "Google",
-    },
-    nocors: ["https://yuansou.netlify.app/"],
-    历史记录设置: {
-        保留历史记录: true,
-        自动清除历史记录: false,
-        d: 14,
-        h: 0,
-    },
-    ding_dock: [0, 0],
-    贴图: {
-        窗口: {
-            变换: `transform: rotateY(180deg);`,
-            双击: "归位",
-        },
-    },
-    代理: {
-        mode: "direct",
-        pacScript: "",
-        proxyRules: "",
-        proxyBypassRules: "",
-    },
-    主页面大小: [800, 600, false],
-    关闭窗口: {
-        失焦: { 主页面: false },
-    },
-    时间格式: "MM/DD hh:mm:ss",
-    硬件加速: true,
-    更新: {
-        检查更新: true,
-        频率: "setting",
-        dev: false,
-        上次更新时间: 0,
-    },
-    录屏: {
-        自动录制: 3,
-        视频比特率: 2.5,
-        摄像头: {
-            默认开启: false,
-            记住开启状态: false,
-            镜像: false,
-        },
-        音频: {
-            默认开启: false,
-            记住开启状态: false,
-        },
-        转换: {
-            ffmpeg: "",
-            自动转换: false,
-            格式: "webm",
-            码率: 2.5,
-            帧率: 30,
-            其他: "",
-            高质量gif: false,
-        },
-        提示: {
-            键盘: {
-                开启: false,
-            },
-            鼠标: {
-                开启: false,
-            },
-            光标: {
-                开启: false,
-                样式: "width: 24px;\nheight: 24px;\nborder-radius: 50%;\nbackground-color: #ff08;",
-            },
-        },
-    },
-    插件: { 加载前: [], 加载后: [] },
+    firstRun: false,
+    settingVersion: app.getVersion(),
 };
-try {
-    default_setting.保存.保存路径.图片 = app.getPath("pictures");
-    default_setting.保存.保存路径.视频 = app.getPath("videos");
-} catch (e) {
-    console.error(e);
-}
 
 function set_default_setting() {
     for (let i in default_setting) {
@@ -800,7 +526,7 @@ function set_default_setting() {
 
 // 增加设置项后，防止undefined
 function fix_setting_tree() {
-    if (store.get("设置版本") == app.getVersion()) return;
+    if (store.get("settingVersion") == app.getVersion()) return;
     var tree = "default_setting";
     walk(tree);
     function walk(path: string) {
