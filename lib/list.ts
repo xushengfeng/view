85005d0e4089a344d0d42b4adcf33050f8961088
xushengfeng <xushengfeng_zg@163.com>
2023-07-04 15:53:15 +0800

    依赖 虚拟列表

---
+++ lib/list.ts
@@ -0,0 +1,25 @@
+function list(pel: HTMLElement, height: number, gap: number, list: any[], render: (list: any) => HTMLElement) {
+    let div = document.createElement("div");
+    function cal() {
+        let top = pel.scrollTop;
+        let bottom = top + pel.offsetHeight;
+
+        let start = 0,
+            end = 0;
+        start = Math.floor((top - gap / 2) / (height + gap));
+        end = Math.ceil((bottom + gap / 2) / (height + gap));
+
+        div.innerHTML = "";
+        for (let i = start; i <= end; i++) {
+            let el = render(list[i]);
+            div.append(el);
+            el.style.height = i * (height + gap) + "px";
+        }
+    }
+    cal();
+    pel.append(div);
+    div.style.height = list.length * (height + gap) - gap + "px";
+    pel.onscroll = cal;
+}
+
+export default list;
