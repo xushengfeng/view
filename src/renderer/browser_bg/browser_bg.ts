c08ffdccfe0b6714ed8db80fde140f915a75b6e8
xushengfeng <xushengfeng_zg@163.com>
2024-10-31 16:10:45 +0800

    错误界面 使用dkh

--- src/renderer/browser_bg/browser_bg.ts
+++ src/renderer/browser_bg/browser_bg.ts
@@ -1,26 +1,42 @@
-const h1 = document.querySelector("h1");
+import { ele, p, pack, pureStyle, view } from "dkh-ui";
+
+pureStyle();
+
+pack(document.body).style({
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+});
+
+const h1 = ele("h1")
+    .bindSet((m: string, el) => {
+        el.innerText = m;
+    })
+    .addInto();
+
+const details = view().addInto();
 
 const search = new URLSearchParams(location.search);
 
 if (navigator.onLine) {
     switch (search.get("type")) {
         case "did-fail-load":
-            h1.innerText = "加载失败";
-            document.getElementById("details").innerHTML = `<p>${"错误代码："}${search.get("err_code")}</p>
-            <p>${"错误描述："}${search.get("err_des")}</p>`;
+            h1.sv("加载失败");
+            details.add([p(`${"错误代码："}${search.get("err_code")}`), p(`${"错误描述："}${search.get("err_des")}`)]);
             break;
         case "render-process-gone":
-            h1.innerText = "进程崩溃";
+            h1.sv("进程崩溃");
             break;
         case "unresponsive":
-            h1.innerText = "页面未响应";
+            h1.sv("页面未响应");
             break;
         case "certificate-error":
-            h1.innerText = "证书错误";
+            h1.sv("证书错误");
             break;
         default:
             break;
     }
 } else {
-    h1.innerText = "无网络连接";
+    h1.sv("无网络连接");
 }
